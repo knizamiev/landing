9b5e696cac1eb360778d77ac57f619a05faeb757
knizamiev <tortiksladko@gmail.com>
2020-08-07 02:36:07 +0300

    asdasd

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import './main.css'
-import Logo from '../assets/7kRQ3hNlrnc.jpg';
+import Logo from '../assets/background-gradient.jpg';
 
 const Main = () => {
 
     return (
-        <div>
+<div>
         <div className='logo'>
             <img src={Logo} alt=""/>
+            <div className='info'>
+                <p>ps</p>
+                <p>best logo og logos</p>
+            </div>
         </div>
-        </div>
+
+    <div className='aboutUs'>
+        <div className='item'>1</div>
+        <div className='item'>2</div>
+        <div className='item'>3</div>
+
+    </div>
+</div>
     )
 
 };
