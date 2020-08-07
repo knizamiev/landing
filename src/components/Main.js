31fdf6cee75260c4d0ea2a8989d1cfdcfd95ad2c
knizamiev <kamil.nizamiev@finstarlabs.ru>
2020-08-07 12:36:41 +0300

    test2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,58 @@
 import React from 'react';
 import './main.css'
 import Logo from '../assets/background-gradient.jpg';
+import Mall from '../assets/mall.jpg';
+
+
 
 const Main = () => {
 
-    return (
-<div>
-        <div className='logo'>
-            <img src={Logo} alt=""/>
-            <div className='info'>
-                <p>ps</p>
-                <p>best logo og logos</p>
-            </div>
-        </div>
+    const [state, setState] = React.useState({loaded: false});
 
-    <div className='aboutUs'>
-        <div className='item'>1</div>
-        <div className='item'>2</div>
-        <div className='item'>3</div>
+    // const Logo = '../assets/background-gradient.jpg';
+    // const Mall = '../assets/mall.jpg';
 
-    </div>
-</div>
-    )
+    // React.useEffect(()=>{
+    //     let imageList = [Logo, Mall];
+    //     console.log(imageList);
+    //     imageList.forEach((image) => {
+    //         new Image().src = image
+    //     });
+    //
+    //     imageList.onload=()=>{
+    //         setState({ imageIsReady: true });
+    //     };
+    //
+    //
+    // },[state]);
+
+    const onImageLoaded = () => {
+        setState({loaded: true });
+    };
+
+    const { loaded } = state;
+
+      console.log('loaded ',loaded)
+            return (
+                <div>
+                    <div className='logo'>
+                        <img src={Logo} on onLoad={onImageLoaded}/>
+                        <div className='info'>
+                            <p>ps</p>
+                            <p>best logo og logos</p>
+                        </div>
+                    </div>
+
+                    <div className='aboutUs'>
+
+                        <img src={Mall} />
+                        <div className='item1'></div>
+                        <div className='item2'>2</div>
+                        <div className='item3'>3</div>
+                    </div>
+                </div>
+            )
+
 
 };
-
 export default Main;
