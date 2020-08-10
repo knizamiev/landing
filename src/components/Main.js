03f70ff24443626bfe91768c359e3ff95bda6b14
knizamiev <kamil.nizamiev@finstarlabs.ru>
2020-08-10 18:02:31 +0300

    new picture

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './main.css'
-import Logo from '../assets/background-gradient.jpg';
+import Logo from '../assets/Slice_2.png';
 import Mall from '../assets/mall.jpg';
+import Join from '../assets/find-us-2880x1440px-compressor.jpg';
 import {Img} from 'react-image'
 
 
@@ -20,14 +21,9 @@ const Main = () => {
 
             return (
                 <div>
-                    {      console.log('loaded ',loaded)
-                    }
                     <div className='logo'>
                         <Img src={Logo}/>
-                        <div className='info'>
-                            <p>ps</p>
-                            <p>best logo og logos</p>
-                        </div>
+
                     </div>
 
                     <div className='aboutUs'>
@@ -36,6 +32,19 @@ const Main = () => {
                         <div className='item1' />
                         <div className='item2'>2</div>
                         <div className='item3'>3</div>
+                    </div>
+                    <div className='join'>
+                        <img src={Join} />
+                        <div className='item_1'>
+                            <div className='item_2'>
+                            <span>Присоединяйся!</span>
+                            </div>
+                            <div className='item_3'>
+                            <span>Это</span>
+                            <span>Отличная</span>
+                            <span>Возможность:</span>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )
